56d3c1e35d13008d89b4a189f22a76704c803ba8
Davidihl <daih1985@gmail.com>
2023-08-28 13:35:15 +0200

    Add collapsing behaviour

--- material-responsive-sidenav/src/app/app.component.ts
+++ material-responsive-sidenav/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-responsive-sidenav';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+  isCollapsed = false;
+
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches){
+        this.isMobile = true;
+        this.sidenav.mode = "over";
+        this.isCollapsed = false; // the menu should never be collapsed on mobile
+      } else {
+        this.isMobile = false;
+        this.sidenav.mode = "side";
+        this.sidenav.open(); // the menu should never be closed on desktop/tablet
+      }
+    });
+  }
+
+  toggleMenu() {
+    if(this.isMobile){
+      this.sidenav.toggle();
+    } else {
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
